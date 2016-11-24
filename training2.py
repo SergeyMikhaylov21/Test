08f721fc693e93c5147d0b9ca99ee4a99dfe7966
Sergey Mikhaylov <Serdgh2011@yandex.ru>
2016-11-24 19:45:29 +0300

    Add files via upload

--- training2.py
+++ training2.py
@@ -1,14 +1,9 @@
 import re
-reg = '[а-яёА-ЯЁ]'
+reg = '(б[аоуыиюяёэ])'
 with open ('C:\\Users\\student\\Desktop\\newtext.txt', 'r', encoding='utf-8') as f:
     s = f.read()
     words = s.split()
-    count = len(words)
-    i = 0
     for word in words:
         if re.search (reg, word):
-            i += 1
-    if count != i:
-        print (i, count)
-    else:
-        print (count)
+            res = re.search (reg, word)
+            print (res.group(1))
